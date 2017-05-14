390429b9041b0641e282a774ef49d0d77d068d86
haquem1 <maroof.haque@gmail.com>
2017-05-14 10:41:17 -0700

    backup

---
+++ Backup/BackupDatabase.sql
@@ -0,0 +1,2 @@
+BACKUP DATABASE [s17guest06] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL11.CSDB440\MSSQL\Backup\s17guest06.bak' WITH NOFORMAT, NOINIT,  NAME = N's17guest06-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
